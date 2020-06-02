102e88ea3e2a445fe51e8785a0ddbc3f86d2d225
RobertPetersen2 <robertpetersen2@gmail.com>
2020-06-02 14:20:39 +0200

    image test

---
+++ OxbridgeApp/Views/CustomViews/CustomViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace OxbridgeApp.Views.CustomViews
+{
+    public partial class CustomViewCell : ViewCell
+    {
+        public CustomViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
